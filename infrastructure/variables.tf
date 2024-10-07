6c401443decd5c1fcf6af4453caa0269286d5c0b
Ahmed Kamal <ahmedkamal200427@gmail.com>
2024-10-07 22:58:44 +0300

    fix no required provider error

---
+++ infrastructure/variables.tf
@@ -0,0 +1,33 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "aks-cluster-resource-group"
+}
+
+variable "kubernetes-cluster-name" {
+  type    = string
+  default = "aks-cluster"
+}
+
+variable "node_count" {
+  type        = number
+  description = "The initial quantity of nodes for the node pool."
+  default     = 3
+}
+
+variable "msi_id" {
+  type        = string
+  description = "The Managed Service Identity ID. Set this value if you're running this example using Managed Identity as the authentication method."
+  default     = null
+}
+
+variable "username" {
+  type        = string
+  description = "The admin username for the new cluster."
+  default     = "azureadmin"
+}
